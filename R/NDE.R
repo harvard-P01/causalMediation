76446be06a09d3702bab7ea197c6f1b3c688a654
Egge van der Poel <info@eggewel.nl>
2016-01-29 20:57:32 +0100

    added functions for pure NIE and total NDE

--- R/NDE.R
+++ R/NDE.R
@@ -14,6 +14,27 @@ NDE_binbin <- function(betas, thetas, treatment, mediator, covariates, cval,
   interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep=':')]),
                             0, thetas[paste(treatment, mediator, sep=':')])  
   
+  ORnde <- (exp(thetas[treatment]*a) * (1+exp(thetas[mediator] +interactionTerm*a+betas[1]+betas[treatment]*a+covariatesTerm)))/
+    (exp(thetas[treatment]*a_star)*(1+exp(thetas[mediator]+interactionTerm*a_star+betas[1]+betas[treatment]*a+covariatesTerm)))
+  unname(ORnde)
+}
+
+total_NDE_binbin <- function(betas, thetas, treatment, mediator, covariates, cval,
+                       a_star = 0, a = 1, interaction = TRUE) {
+  covariatesTerm <- 0
+  if (is.null(cval)) {
+    for (c in covariates){
+      covariatesTerm <- covariatesTerm + betas[c] * apply(df[c], 2, mean, na.rm=TRUE)
+    }
+  } else {
+    for (i in 1:length(covariates)) {
+      covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
+    }
+  }
+  
+  interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep=':')]),
+                            0, thetas[paste(treatment, mediator, sep=':')])  
+  
   ORnde <- (exp(thetas[treatment]*a) * (1+exp(thetas[mediator] +interactionTerm*a+betas[1]+betas[treatment]*a_star+covariatesTerm)))/
     (exp(thetas[treatment]*a_star)*(1+exp(thetas[mediator]+interactionTerm*a_star+betas[1]+betas[treatment]*a_star+covariatesTerm)))
   unname(ORnde)
@@ -36,6 +57,27 @@ NDE_bincont <- function(betas, thetas, treatment, mediator, covariates, cval,
   
   nde <- thetas[treatment]*(a-a_star)+interactionTerm*(a - a_star)*(exp(betas[1]+betas[treatment]*a_star+covariatesTerm)/
                                                                       (1+exp(betas[1]+betas[treatment]*a_star+covariatesTerm)))
+  
+  unname(nde)
+}
+
+total_NDE_bincont <- function(betas, thetas, treatment, mediator, covariates, cval,
+                        a_star = 0, a = 1, interaction = TRUE) {
+  covariatesTerm <- 0
+  if (is.null(cval)) {
+    for (c in covariates){
+      covariatesTerm <- covariatesTerm + betas[c] * apply(df[c], 2, mean, na.rm=TRUE)
+    }
+  } else {
+    for (i in 1:length(covariates)) {
+      covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
+    }
+  }
+  
+  interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep=':')]),0,thetas[paste(treatment, mediator, sep=':')])  
+  
+  nde <- thetas[treatment]*(a-a_star)+interactionTerm*(a - a_star)*(exp(betas[1]+betas[treatment]*a+covariatesTerm)/
+                                                                      (1+exp(betas[1]+betas[treatment]*a+covariatesTerm)))
   
   unname(nde)
 }
@@ -103,8 +145,27 @@ NDE_contcont <- function(betas, thetas, treatment, mediator, covariates, cval,
   unname(nde)
 }
 
+total_NDE_contcont <- function(betas, thetas, treatment, mediator, covariates, cval,
+                         a_star = 1, a = 0, interaction = TRUE){
+  covariatesTerm <- 0
+  if (is.null(cval)) {
+    for (c in covariates){
+      covariatesTerm <- covariatesTerm + betas[c] * apply(df[c], 2, mean, na.rm=TRUE)
+    }
+  } else {
+    for (i in 1:length(covariates)) {
+      covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
+    }
+  }
+  interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep = ':')]),
+                            0, thetas[paste(treatment, mediator, sep = ':')])  
+  
+  nde <- (thetas[treatment] + interactionTerm * betas[1] + interactionTerm * betas[treatment] * a + interactionTerm*covariatesTerm)*(a - a_star)
+  unname(nde)
+}
 
-NDE_contcont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
+
+total_NDE_contcont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
   ### vecc = vector of covariates
   ### DEBUG: for testing purposes
   #thetas <- c(1,2,3,4)
@@ -121,7 +182,7 @@ NDE_contcont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
   
   f <- "x2"
   if(interaction){
-    f <- paste0("(", f, " + x", k, "*x", k+1, " + x", k, "*x", k+2, "*a_star + ")
+    f <- paste0("(", f, " + x", k, "*x", k+1, " + x", k, "*x", k+2, "*a + ")
     
     fc <- paste0("x", k + 2 + 1:j, "  * ", "vecc_", 1:j, collapse = " + ")
     fc <- paste0("x",k," *(", fc, ")")
@@ -225,7 +286,7 @@ total_NDE_contbin_delta <- function(thetas, vecc, variance, interaction = TRUE, 
   return(as.formula(f))
 }
 
-NDE_bincont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
+total_NDE_bincont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
   ### vecc = vector of covariates
   ### DEBUG: for testing purposes
   #thetas <- c(1,2,3,4)
@@ -246,11 +307,11 @@ NDE_bincont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
     
     # Numerator
     N2 <- paste0("x", k+1:2, collapse = " + " )
-    N2 <- paste0("exp(", N2, "*a_star + ")
+    N2 <- paste0("exp(", N2, "*a + ")
     N2 <- paste(N2, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
     # Denominator
     D2 <- paste0("x", k+1:2, collapse = " + " )
-    D2 <- paste0("1 + exp(", D2, "*a_star + ")
+    D2 <- paste0("1 + exp(", D2, "*a + ")
     D2 <- paste(D2, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
     D2 <- paste0("(", D2, ")")
     
@@ -307,6 +368,61 @@ NDE_binbin_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
     D2 <- paste0("(1+exp(x3 + x",k,"*a_star + x",k+1," + x", k+2, "*a_star + ", paste0("x", k + 2 + 1:j, "*", "vecc_", 1:j, collapse = " + "), "))")
   }else{
     D2 <- paste0("(1+exp(x3 + x",                k+1," + x", k+2, "*a_star + ", paste0("x", k + 2 + 1:j, "*", "vecc_", 1:j, collapse = " + "), "))")
+  }
+  
+  # Construct denominator
+  D <- paste(D1, D2, sep="*")
+  
+  # Construct formula
+  f <- paste0(" ~ (",N,"/",D,")")
+  
+  ### DEBUG: for testing purposes
+  if(debug){
+    print("DEBUG: NDE_bincont_delta")
+    print(paste0("DEBUG: length(thetas) = ", length(thetas)))
+    print(paste0("DEBUG: length(vecc)   = ", length(vecc)))
+    print(paste0("DEBUG: formula = ", f))
+  }
+  
+  return(as.formula(f))
+}
+
+total_NDE_binbin_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
+  ### vecc = vector of covariates
+  ### DEBUG: for testing purposes
+  #thetas <- c(1,2,3,4)
+  #vecc   <- c(1,2)
+  #interaction=FALSE
+  #debug=TRUE
+  
+  j <- length(vecc)
+  k <- length(thetas)
+  
+  for (i in 1:j){
+    assign(paste("vecc", i, sep = "_"), vecc[i])
+  }
+  
+  # First term in nominator
+  N1 <- "exp(x2*a)"
+  
+  # Second term in nominator
+  if(interaction){
+    N2 <- paste0("(1+exp(x3 + x",k,"*a + x",k+1," + x", k+2, "*a + ", paste0("x", k + 2 + 1:j, "*", "vecc_", 1:j, collapse = " + "), "))")
+  }else{
+    N2 <- paste0("(1+exp(x3 + x",           k+1," + x", k+2, "*a + ", paste0("x", k + 2 + 1:j, "*", "vecc_", 1:j, collapse = " + "), "))")
+  }
+  
+  # Construct nominator
+  N <- paste(N1, N2, sep="*")
+  
+  # First term in denominator
+  D1 <- "exp(x2*a_star)"
+  
+  # Second term in denominator
+  if(interaction){
+    D2 <- paste0("(1+exp(x3 + x",k,"*a_star + x",k+1," + x", k+2, "*a + ", paste0("x", k + 2 + 1:j, "*", "vecc_", 1:j, collapse = " + "), "))")
+  }else{
+    D2 <- paste0("(1+exp(x3 + x",                k+1," + x", k+2, "*a + ", paste0("x", k + 2 + 1:j, "*", "vecc_", 1:j, collapse = " + "), "))")
   }
   
   # Construct denominator
--- R/NIE.R
+++ R/NIE.R
@@ -10,13 +10,35 @@ NIE_binbin <- function(betas, thetas, treatment, mediator, covariates, cval,
       covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
     }
   }
-    
+  
   interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep = ':')]),
                             0,
                             thetas[paste(treatment, mediator, sep = ':')])
   ORnie <- (1 + exp(betas[1] + betas[treatment] * a_star + covariatesTerm) *
               (1 + exp(thetas[mediator] + interactionTerm * a + betas[1] + betas[treatment] * a + covariatesTerm)))/
     ((1 + exp(betas[1] + betas[treatment]*a+covariatesTerm))*(1 + exp(thetas[mediator] + interactionTerm * a + betas[1] + betas[treatment] * a_star + covariatesTerm)))
+  unname(ORnie)
+}
+
+pure_NIE_binbin <- function(betas, thetas, treatment, mediator, covariates, cval,
+                       a_star = 0, a = 1, interaction = TRUE) {
+  covariatesTerm <- 0
+  if (is.null(cval)) {
+    for (c in covariates){
+      covariatesTerm <- covariatesTerm + betas[c] * apply(df[c], 2, mean, na.rm=TRUE)
+    }
+  } else {
+    for (i in 1:length(covariates)) {
+      covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
+    }
+  }
+  
+  interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep = ':')]),
+                            0,
+                            thetas[paste(treatment, mediator, sep = ':')])
+  ORnie <- (1 + exp(betas[1] + betas[treatment] * a_star + covariatesTerm) *
+              (1 + exp(thetas[mediator] + interactionTerm * a_star + betas[1] + betas[treatment] * a + covariatesTerm)))/
+    ((1 + exp(betas[1] + betas[treatment]*a+covariatesTerm))*(1 + exp(thetas[mediator] + interactionTerm * a_star + betas[1] + betas[treatment] * a_star + covariatesTerm)))
   unname(ORnie)
 }
 
@@ -38,6 +60,28 @@ NIE_bincont <- function(betas, thetas, treatment, mediator, covariates, cval,
                             thetas[paste(treatment, mediator, sep=':')])  
   
   nie <- (thetas[mediator]+interactionTerm*a) *
+    (exp(betas[1] + betas[treatment] * a + covariatesTerm) / (1 + exp(betas[1] + betas[treatment] * a + covariatesTerm)) -
+       exp(betas[1] + betas[treatment] * a_star + covariatesTerm) / (1 + exp(betas[1] + betas[treatment] * a_star + covariatesTerm)))
+  unname(nie)
+}
+
+pure_NIE_bincont <- function(betas, thetas, treatment, mediator, covariates, cval,
+                        a_star = 0, a = 1, interaction = TRUE) {
+  covariatesTerm <- 0
+  if (is.null(cval)) {
+    for (c in covariates){
+      covariatesTerm <- covariatesTerm + betas[c] * apply(df[c], 2, mean, na.rm=TRUE)
+    }
+  } else {
+    for (i in 1:length(covariates)) {
+      covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
+    }
+  }
+  interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep=':')]),
+                            0,
+                            thetas[paste(treatment, mediator, sep=':')])  
+  
+  nie <- (thetas[mediator]+interactionTerm*a_star) *
     (exp(betas[1] + betas[treatment] * a + covariatesTerm) / (1 + exp(betas[1] + betas[treatment] * a + covariatesTerm)) -
        exp(betas[1] + betas[treatment] * a_star + covariatesTerm) / (1 + exp(betas[1] + betas[treatment] * a_star + covariatesTerm)))
   unname(nie)
@@ -99,6 +143,26 @@ NIE_contcont <- function(betas, thetas, treatment, mediator, covariates, cval,
                             0,
                             thetas[paste(treatment, mediator, sep=':')])  
   
+  nie <- (thetas[mediator] * betas[treatment] + interactionTerm * betas[treatment] * a) * (a - a_star)  
+  unname(nie)
+}
+
+pure_NIE_contcont <- function(betas, thetas, treatment, mediator, covariates, cval,
+                         a_star = 0, a = 1, interaction = TRUE) {
+  covariatesTerm <- 0
+  if (is.null(cval)) {
+    for (c in covariates){
+      covariatesTerm <- covariatesTerm + betas[c] * apply(df[c], 2, mean, na.rm=TRUE)
+    }
+  } else {
+    for (i in 1:length(covariates)) {
+      covariatesTerm <- covariatesTerm + betas[covariates[i]] * cval[i]
+    }
+  }
+  interactionTerm <- ifelse(is.na(thetas[paste(treatment, mediator, sep=':')]),
+                            0,
+                            thetas[paste(treatment, mediator, sep=':')])  
+  
   nie <- (thetas[mediator] * betas[treatment] + interactionTerm * betas[treatment] * a_star) * (a - a_star)  
   unname(nie)
 }
@@ -117,9 +181,36 @@ NIE_contcont_delta <- function(thetas, interaction=TRUE, debug=FALSE){
   if(interaction){
     F2 <- paste0(" + x", k, " * x", k+2, " * a)")
   }
-
+  
   f = paste0(" ~ ", F1, F2, F3)
+  
+  ### DEBUG: for testing purposes
+  if(debug){
+    print("DEBUG: NIE_contcont_delta")
+    print(paste0("DEBUG: length(thetas) = ", length(thetas)))
+    print(paste0("DEBUG: formula = ", f))
+  }  
+  
+  return(as.formula(f))
+}
 
+pure_NIE_contcont_delta <- function(thetas, interaction=TRUE, debug=FALSE){
+  ### DEBUG: for testing purposes
+  #thetas <- c(1,2,3,4)
+  #interaction=FALSE
+  
+  k <- length(thetas)
+  
+  F1 <- paste0("(x3 * x", k+2)
+  F2 <- ")"
+  F3 <- " * (a-a_star)"
+  
+  if(interaction){
+    F2 <- paste0(" + x", k, " * x", k+2, " * a_star)")
+  }
+  
+  f = paste0(" ~ ", F1, F2, F3)
+  
   ### DEBUG: for testing purposes
   if(debug){
     print("DEBUG: NIE_contcont_delta")
@@ -206,7 +297,59 @@ NIE_bincont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
   N1 <- paste0("x", k+1:2, collapse = " + " )
   N1 <- paste0("exp(", N1, "*a + ")
   N1 <- paste(N1, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
+  
+  # Second numerator
+  N2 <- paste0("x", k+1:2, collapse = " + " )
+  N2 <- paste0("exp(", N2, "*a_star + ")
+  N2 <- paste(N2, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
+  
+  # First denominator
+  D1 <- paste0("x", k+1:2, collapse = " + " )
+  D1 <- paste0("1 + exp(", D1, "*a + ")
+  D1 <- paste(D1, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
+  D1 <- paste0("(", D1, ")")
+  
+  # Second denominator
+  D2 <- paste0("x", k+1:2, collapse = " + " )
+  D2 <- paste0("1 + exp(", D2, "*a_star + ")
+  D2 <- paste(D2, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
+  D2 <- paste0("(", D2, ")")
+  
+  # Construct formula
+  f <- paste0(" ~ ", F1, " * ( (", N1, "/", D1, ") - (", N2, "/", D2, ") )")
+  
+  ### DEBUG: for testing purposes
+  if(debug){
+    print("DEBUG: NIE_bincont_delta")
+    print(paste0("DEBUG: length(thetas) = ", length(thetas)))
+    print(paste0("DEBUG: length(vecc)   = ", length(vecc)))
+    print(paste0("DEBUG: formula = ", f))
+  }
+  
+  return(as.formula(f))
+}
 
+pure_NIE_bincont_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
+  ### vecc = vector of covariates
+  ### DEBUG: for testing purposes
+  #thetas <- c(1,2,3,4)
+  #vecc   <- c(1,2)
+  #interaction=FALSE
+  
+  j <- length(vecc)
+  k <- length(thetas)
+  
+  for (i in 1:j){
+    assign(paste("vecc", i, sep = "_"), vecc[i])
+  }
+  
+  F1 <- paste0("(x3 + x", k, "*a_star)")
+  
+  # First numerator
+  N1 <- paste0("x", k+1:2, collapse = " + " )
+  N1 <- paste0("exp(", N1, "*a + ")
+  N1 <- paste(N1, paste0("x", k + 2 + 1:j, " * ", "vecc_", 1:j, collapse = " + "), ")")
+  
   # Second numerator
   N2 <- paste0("x", k+1:2, collapse = " + " )
   N2 <- paste0("exp(", N2, "*a_star + ")
@@ -278,6 +421,53 @@ NIE_binbin_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
   ### DEBUG: for testing purposes
   if(debug){
       print("DEBUG: NIE_binbin_delta")
+    print(paste0("DEBUG: length(thetas) = ", length(thetas)))
+    print(paste0("DEBUG: length(vecc)   = ", length(vecc)))
+    print(paste0("DEBUG: formula = ", f))
+  }
+  
+  return(as.formula(f))
+}
+
+pure_NIE_binbin_delta <- function(thetas, vecc, interaction = TRUE, debug=FALSE) {
+  ### vecc = vector of covariates
+  ### DEBUG: for testing purposes
+  #thetas <- c(1,2,3,4)
+  #vecc   <- c(1,2)
+  #interaction=FALSE
+  
+  j <- length(vecc)
+  k <- length(thetas)
+  
+  ### FIXME: it seems that only X2 differs with/without interaction 
+  ###        shall we get the other terms out of the if/else statement?
+  
+  ### QUESTION: Why do we need to specify xk all the time and x3 can remain like that? 
+  ###           Doesn't it matter if extra terms are added to the thetas vector?
+  
+  for (i in 1:j){
+    assign(paste("vecc", i, sep = "_"), vecc[i])
+  }
+  
+  X1 <- paste0("x", k + 1:2, collapse = " + ")
+  XC <- paste0("x", k + 2 + 1:j, "  * ", "vecc_", 1:j, collapse = " + ")
+  s1 <- paste0("(1 + exp(", X1, " * a_star + ", XC, "))")
+  s3 <- paste0("(1 + exp(", X1, " * a + ", XC, "))")
+  s4 <- paste0("(1 + exp(", X2, " + ", X1, " * a_star +", XC, "))")
+  
+  if (interaction) {
+    X2 <- paste0("x3 +x",k)
+    s2 <- paste0("(1 + exp(", X2, " * a_star + ", X1, " * a +", XC, "))")
+  } else {
+    X2 <- paste0("x3")
+    s2 <- paste0("(1 + exp(", X2, " + ", X1, " * a +", XC, "))")
+  }
+  
+  f <- paste0(" ~ ", "(", s1, "*", s2, ")/(", s3, "*", s4, ")")
+  
+  ### DEBUG: for testing purposes
+  if(debug){
+    print("DEBUG: NIE_binbin_delta")
     print(paste0("DEBUG: length(thetas) = ", length(thetas)))
     print(paste0("DEBUG: length(vecc)   = ", length(vecc)))
     print(paste0("DEBUG: formula = ", f))
