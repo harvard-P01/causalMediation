be053190c7149f3bd60b13d52c5ada68eb6dd30b
Egge van der Poel <info@eggewel.nl>
2016-01-30 20:21:59 +0100

    initial commit of PM.R

---
+++ R/PM.R
@@ -0,0 +1,13 @@
+# PLEASE, GIVE IT A TRY. IT SHOULD BE:
+  # WHEN Y CONT: PM=TNIE/(PNDE+TE)
+  # WHEN Y NOT CONT: PM=(PNDE*(TNIE-1))/(PNDE*TNIE-1)
+
+pm <- function(pnde, tnie, te, ycont=FALSE){
+  res <- 0
+  if(ycont){
+    res <- tnie/(pnde+te)
+  }else{
+    res <- (pnde*(tnie-1))/(pnde*tnie-1)
+  }
+  res  
+}
