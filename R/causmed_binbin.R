8bc8a94707496f2c34582bfeda4bd02c65623da4
cchoirat <cchoirat@gmail.com>
2016-12-28 14:54:29 -0500

    Removed files from previous version

--- R/causmed_binbin.R
+++
@@ -1,6 +0,0 @@
-causmed_binbin <- setRefClass("causmed",
-                              fields = list(
-                                CDE_bin = "function",
-                                CDE_bin_delta = "function")
-)
-
