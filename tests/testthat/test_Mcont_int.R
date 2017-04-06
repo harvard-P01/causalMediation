5169b67be1d6da3cbe1a3cb4b8c6c9fb4f0e501f
Christine Choirat <cchoirat@gmail.com>
2017-04-06 18:23:44 +0200

    contint

--- tests/testthat/test_Mcont_int.R
+++ tests/testthat/test_Mcont_int.R
@@ -1,67 +1,69 @@
-# library(causalMediation)
+set.seed(1234)
+
+f <- function(outcome = "Y_cont_int", yreg = "linear",
+              file_name = "Ycont", folder = "Mcont_int",
+              event = NULL, casecontrol = FALSE, data, mediation = TRUE) {
+  cm <- causmed$new(data = data,
+                    outcome = outcome,
+                    treatment = "A",
+                    mediator = "M_cont",
+                    covariates = "C",
+                    vec = 1,
+                    m = 0,
+                    interaction = TRUE,
+                    event = event,
+                    casecontrol = casecontrol,
+                    yreg = yreg, mreg = "linear",
+                    boot = TRUE, nboot = 100)
+  
+  files <- paste0(paste0(folder, "/", file_name, c("_delta.txt", "_boot.txt", "_mediate.txt")))
+  
+  cm$delta_marginal()
+  cm$delta_conditional()
+  # cm$print_output(type = "full")
+  sink(files[1])
+  cm$print_output(type = "full")
+  sink()
+  
+  cm$bootstrap_marginal()
+  cm$bootstrap_conditional()
+  # cm$print_output(type = "full")
+  sink(files[2])
+  cm$print_output(type = "full")
+  sink()
+  
+  if (mediation) {
+    cm$mediation()
+    sink(files[3])
+    print(cm$mediation())
+    sink()
+  }
+  
+  return(cm)
+}
+
+# data(Mbin_int_data)
+
+# Mbin_int_data <- read.csv("../../inst/data/Mbin_int_data_10000.csv")
+d <- read.csv("../../inst/data/Mcont_int_data.csv")
+o <- f(outcome = "Y_cont_int", yreg = "linear", file_name = "Ycont", folder = "Mcont_int", data = d)
+# o <- f(outcome = "Y_bin_int", yreg = "loglinear", file_name = "Yloglin", folder = "Mcont_int", data = d)
+# o <- f(outcome = "Y_bin_int", yreg = "logistic", file_name = "Ybin", folder = "Mcont_int", data = d)
+# o <- f(outcome = "Y_count_int", yreg = "quasipoisson", file_name = "Yqpoi", folder = "Mcont_int", data = d)
+# o <- f(outcome = "Y_count_int", yreg = "poisson", file_name = "Ypoi", folder = "Mcont_int", data = d)
+# o <- f(outcome = "Y_count_int", yreg = "negbin", file_name = "Ynegbin", folder = "Mcont_int", data = d)
 # 
-# # df <- read.csv("data/Mcont_int_data.txt", sep = " ")
-# df <- read.csv("data/Mcont_int_data_10000.txt", sep = " ")
+# Mbin_int_data$event <- 1 - Mbin_int_data$delta
+# o <- f(outcome = "Ycen_int", yreg = "coxph", file_name = "Ycox", event = "event", folder = "Mbin_int", data = Mbin_int_data, mediation = FALSE)
+# o <- f(outcome = "Ycen_int", yreg = "aft_exp", file_name = "Yexp", event = "event", folder = "Mbin_int", data = Mbin_int_data, mediation = FALSE)
 # 
-# ##----- Bootstrap - Y_cont_int
+# # data(Mbin_wei_int_data)
+# Mbin_wei_int_data <- read.csv("../../inst/data/Mbin_wei_int_data.csv")
+# Mbin_wei_int_data$event <- 1 - Mbin_wei_int_data$delta
+# o <- f(outcome = "Ycen_int", yreg = "aft_weibull", file_name = "Ywei", event = "event", folder = "Mbin_int", data = Mbin_wei_int_data, mediation = FALSE)
 # 
-# set.seed(1234)
-# s_boot_Y_cont_int_M_cont <- causalMediation(data = df,
-#                                               outcome = "Y_cont_int",
-#                                               treatment = 'A',
-#                                               mediator = 'M_cont',
-#                                               covariates = "C",
-#                                               interaction = TRUE,
-#                                               yreg = "linear", mreg = "linear",
-#                                               boot = TRUE, nboot = 500)
-# s_boot_Y_cont_int_M_cont
-# sink(file = "inst/simulations/s_boot_Y_cont_int_M_cont.txt")
-# s_boot_Y_cont_int_M_cont
-# sink()
-# 
-# ##----- Delta - Y_cont_int
-# 
-# s_delta_Y_cont_int_M_cont <- causalMediation(data = df,
-#                                                outcome = "Y_cont_int",
-#                                                treatment = 'A',
-#                                                mediator = 'M_cont',
-#                                                covariates = "C",
-#                                                interaction = TRUE,
-#                                                yreg = "linear", mreg = "linear",
-#                                                boot = FALSE)
-# s_delta_Y_cont_int_M_cont
-# sink(file = "inst/simulations/s_delta_Y_cont_int_M_cont.txt")
-# s_delta_Y_cont_int_M_cont
-# sink()
-# 
-# ##----- Bootstrap - Y_bin_int
-# 
-# set.seed(1234)
-# s_boot_Y_bin_int_M_cont <- causalMediation(data = df,
-#                                              outcome = "Y_bin_int",
-#                                              treatment = 'A',
-#                                              mediator = 'M_cont',
-#                                              covariates = "C",
-#                                              interaction = TRUE,
-#                                              yreg = "logistic", mreg = "linear",
-#                                              boot = TRUE, nboot = 500)
-# s_boot_Y_bin_int_M_cont
-# sink(file = "inst/simulations/s_boot_Y_bin_int_M_cont.txt")
-# s_boot_Y_bin_int_M_cont
-# sink()
-# 
-# ##----- Delta - Y_bin_int
-# 
-# s_delta_Y_bin_int_M_cont <- causalMediation(data = df,
-#                                               outcome = "Y_bin_int",
-#                                               treatment = 'A',
-#                                               mediator = 'M_cont',
-#                                               covariates = "C",
-#                                               interaction = TRUE,
-#                                               yreg = "logistic", mreg = "linear",
-#                                               boot = FALSE)
-# s_delta_Y_bin_int_M_cont
-# sink(file = "inst/simulations/s_delta_Y_bin_int_M_cont.txt")
-# s_delta_Y_bin_int_M_cont
-# sink()
+# # data(Mbin_cc_int_data)
+# Mbin_cc_int_data <- read.csv("../../inst/data/Mbin_cc_int_data.csv")
+# names(Mbin_cc_int_data)[4] <- "M_bin"
+# o <- f(outcome = "Y_bincc_int", yreg = "logistic", file_name = "Ybincc", casecontrol = TRUE, folder = "Mbin_int", data = Mbin_cc_int_data, mediation = FALSE)
 # 
