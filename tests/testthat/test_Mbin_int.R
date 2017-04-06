85f9e5ecf2e0955ac3c003bee61d47a38774b63d
Christine Choirat <cchoirat@gmail.com>
2017-04-06 16:48:17 +0200

    Mbin_int

--- tests/testthat/test_Mbin_int.R
+++ tests/testthat/test_Mbin_int.R
@@ -2,7 +2,7 @@ set.seed(1234)
 
 f <- function(outcome = "Y_cont_int", yreg = "linear",
               file_name = "Ycont", folder = "Mbin_int",
-              event = NULL, casecontrol = FALSE, data) {
+              event = NULL, casecontrol = FALSE, data, mediation = TRUE) {
   cm <- causmed$new(data = data,
                     outcome = outcome,
                     treatment = 'A',
@@ -32,10 +32,12 @@ f <- function(outcome = "Y_cont_int", yreg = "linear",
   cm$print_output(type = "full")
   sink()
   
-  cm$mediation()
-  sink(files[3])
-  print(cm$mediation())
-  sink()
+  if (mediation) {
+    cm$mediation()
+    sink(files[3])
+    print(cm$mediation())
+    sink()
+  }
   
   return(cm)
 }
@@ -49,14 +51,14 @@ f(outcome = "Y_count_int", yreg = "poisson", file_name = "Ypoi", folder = "Mbin_
 f(outcome = "Y_count_int", yreg = "negbin", file_name = "Ynegbin", folder = "Mbin_int", data = Mbin_int_data)
 
 Mbin_int_data$event <- 1 - Mbin_int_data$delta
-f(outcome = "Ycen_int", yreg = "coxph", file_name = "Ycox", event = "event", folder = "Mbin_int", data = Mbin_int_data)
-f(outcome = "Ycen_int", yreg = "aft_exp", file_name = "Yexp", event = "event", folder = "Mbin_int", data = Mbin_int_data)
+f(outcome = "Ycen_int", yreg = "coxph", file_name = "Ycox", event = "event", folder = "Mbin_int", data = Mbin_int_data, mediation = FALSE)
+f(outcome = "Ycen_int", yreg = "aft_exp", file_name = "Yexp", event = "event", folder = "Mbin_int", data = Mbin_int_data, mediation = FALSE)
 
 data(Mbin_wei_int_data)
 Mbin_wei_int_data$event <- 1 - Mbin_wei_int_data$delta
-f(outcome = "Ycen_int", yreg = "aft_weibull", file_name = "Ywei", event = "event", folder = "Mbin_int", data = Mbin_wei_int_data)
+f(outcome = "Ycen_int", yreg = "aft_weibull", file_name = "Ywei", event = "event", folder = "Mbin_int", data = Mbin_wei_int_data, mediation = FALSE)
 
 data(Mbin_cc_int_data)
 names(Mbin_cc_int_data)[4] <- "M_bin"
-f(outcome = "Y_bincc_int", yreg = "logistic", file_name = "Ybincc", casecontrol = TRUE, folder = "Mbin_int", data = Mbin_cc_int_data)
+f(outcome = "Y_bincc_int", yreg = "logistic", file_name = "Ybincc", casecontrol = TRUE, folder = "Mbin_int", data = Mbin_cc_int_data, mediation = FALSE)
 
