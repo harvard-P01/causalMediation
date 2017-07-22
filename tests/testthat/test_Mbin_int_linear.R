f2858761d97a332663ac2218f020321d82c65822
Christine Choirat <cchoirat@gmail.com>
2017-07-22 11:01:55 +0200

    Delta tests only

--- tests/testthat/test_Mbin_int_linear.R
+++ tests/testthat/test_Mbin_int_linear.R
@@ -1,55 +1,29 @@
 d <- read.csv("../../inst/data/Mbin_int_data.csv")
 names(d)
 
-cm <- causmed$new(data = d,
-                  outcome = "Y_cont_int",
-                  treatment = "A",
-                  mediator = "M_bin",
-                  covariates = "C",
-                  vecc = 1,
-                  interaction = TRUE,
-                  boot = FALSE,
-                  nboot = 100,
-                  mreg = "logistic",
-                  yreg = "linear",
-                  event = NULL,
-                  m = 0,
-                  a_star = 0,
-                  a = 1,
-                  casecontrol = FALSE,
-                  baseline = NULL)
 
-files <- paste0(paste0("Mbin_int", "/", "linear",
-                       c("_delta.txt", "_boot.txt", "_mediate.txt", "_medflex.txt")))
+cm <- causmed$new(
+  data = d,
+  outcome = "Y_cont_int",
+  treatment = "A",
+  mediator = "M_bin",
+  covariates = "C",
+  vecc = 1,
+  interaction = TRUE,
+  boot = FALSE,
+  nboot = 100,
+  mreg = "logistic",
+  yreg = "linear",
+  event = NULL,
+  m = 0,
+  a_star = 0,
+  a = 1,
+  casecontrol = FALSE,
+  baseline = NULL
+)
 
-cm$delta_marginal()
-cm$delta_conditional()
-cm$print_output(type = "full")
-sink(files[1])
-cm$print_output(type = "full")
-sink()
-
-cm$bootstrap_marginal()
-cm$bootstrap_conditional()
-cm$print_output(type = "full")
-sink(files[2])
-cm$print_output(type = "full")
-sink()
-
-if (mediation) {
-  cm$mediation()
-  sink(files[3])
-  print(cm$mediation())
-  sink()
-}
-
-if (medflex) {
-  cm$medflex_weight_continuous()
-  sink(files[4])
-  print(cm$medflex_weight_continuous())
-  sink()
-}
-
-sas <- read.csv("../../inst/sasoutput/Mbin_int_linear.csv")
-sas <- sas[!is.na(sas$Obs), ]
-sas
+run_test(
+  cm,
+  filename = "Mbin_int/linear_delta.txt",
+  sas = read.csv("../../inst/sasoutput/Mbin_int_linear_delta.csv")
+)
